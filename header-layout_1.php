3601a07a68d633b0189b085feb6ca11eb1d66365
omagas <omaga1130@gmail.com>
2016-06-15 15:07:44 +0800

    mod INDEX social media icon

--- header-layout_1.php
+++ header-layout_1.php
@@ -10,15 +10,25 @@
         $aglee_pro_header_menu_align = get_theme_mod('header_menu_display_setting', 'menu_align_right');
         
     ?>
+            <div class="social-media-hao">
+                <a href="https://www.facebook.com/Sara%E7%9A%84%E9%A3%9F%E9%A3%9F%E8%AA%B2%E8%AA%B2-234239390013839/" target="_blank">
+                    <img src="http://www.sarasdiyhealth.com/test2/wp-content/uploads/2016/06/icon-fb.png">
+                </a>
+                <a href="https://www.youtube.com/channel/UCa2PO2-QJMf65xCmaAN8xKQ" target="_blank">
+                    <img src="http://www.sarasdiyhealth.com/test2/wp-content/uploads/2016/06/icon-youtube3.png">
+                </a>                
+            </div>    
 	<header id="masthead" class="site-header layout-one <?php echo $show_top_header; ?> <?php echo $aglee_pro_header_menu_align; ?>" role="banner">
-        	<div class="top-header clearfix">
+
+            <div class="top-header clearfix">
                 <div class="ap clearfix">
                     <!-- header top content -->
                     <div class="content-top-head">
                        
                         <?php if(($show_search=get_theme_mod('show_search')) == 0) : ?>
                             <div class="search-icon">
-                                <i class="fa fa-search"></i>
+                                <!--i class="fa fa-search">
+                                </i-->
                                 <div class="aglee-search">
                                          <form action="<?php echo site_url(); ?>" class="search-form" method="get" role="search">
                                             <label>
@@ -63,7 +73,7 @@
                     <?php if($show_header == 'header_logo_only') : ?>
                         <?php if(get_header_image()) : ?>
                             <div class="header-logo-container">
-                                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="http://www.sarasdiyhealth.com/test2/wp-content/uploads/2016/05/cropped-index-final-01_031-1.jpg" /></a></h1>
+                                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="http://www.sarasdiyhealth.com/test2/wp-content/uploads/2016/05/cropped-index-final-01_031-1.png" /></a></h1>
                             </div>
                         <?php endif; ?>
                         <?php elseif($show_header == 'header_text_only') : ?>
